d4df33e212daad98baf2e4c8360b0569fc3c0ef2
Алекс Киасса <chiazza73@gmail.com>
2022-04-21 19:15:19 +0300

    first commit catalogue

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -2,13 +2,16 @@
 
 @section('content')
     <div class="container">
-
-        @foreach ($categories as $category)
-           {{$category->title}}<br>
-                   @foreach($category->children as $childrens)
-                       {{$childrens->title}}
-                   @endforeach
-
+        @foreach($categories as $category)
+            <ul><li>{{$category->title}}
+                    @foreach($category->children as $childrens)
+                        <ul><li>{{$childrens->title}}
+                                @foreach($childrens->children as $children)
+                                    <ul><li>{{$children->title}}</li></ul>
+                                @endforeach
+                            </li></uL>
+                    @endforeach
+                </li></ul>
         @endforeach
     </div>
 @endsection
